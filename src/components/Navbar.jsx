5136b53825b7b543df61d87393a6756d2e2cee8d
Akash <akashmagar7500@gmail.com>
2025-08-27 20:13:40 +0530

    added shadcn

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,15 @@
+import { Button } from "./ui/button";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="flex py-2 items-center justify-between">
+      <h2 className="text-md font-bold">RepAI</h2>
+      <div className="flex gap-4">
+        <Button variant="ghost">Login</Button>
+        <Button>Sign Up</Button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/config/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBQMtp6-bk69WCG7QIw10-EkfSIGv0Dunc",
+  authDomain: "repai-d7fa4.firebaseapp.com",
+  projectId: "repai-d7fa4",
+  storageBucket: "repai-d7fa4.firebasestorage.app",
+  messagingSenderId: "929360936068",
+  appId: "1:929360936068:web:d6ff1dbee6ecd8cd7896d1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
